cbf22a3c6e0bf731be5f8e7ed3e073bc96776b5f
ritushreedas2002 <ritushreedas20027@gmail.com>
2024-11-15 01:27:48 +0530

    mini

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 // const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
-const CSS_URL="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL="https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
 // app.use(
 //   "/api/docs",
 //   swaggerUi.serve,
